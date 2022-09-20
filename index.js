cceb9d99a41ad23a5fe36f6bc01da5ba4a85006e
Maria Sima <simam9520@gmail.com>
2022-09-20 17:28:59 +0300

    index.js

--- index.js
+++ index.js
@@ -1,23 +1,78 @@
+function userAction(tile, index) {
+  console.log(index);
+  tile.textContent = `${currentPlayer}`;
+  changePlayer();
+  // TODO Handle when the user clicks on a tile
+}
+const tiles = Array.from(document.querySelectorAll(".tile"));
+const playerDisplay = document.querySelector(".display-player");
+const resetButton = document.querySelector("#reset");
+const announcer = document.querySelector(".announcer");
 
-/// DO NOT MODIFY THESE LINES
-
-const tiles = Array.from(document.querySelectorAll('.tile'));
-const playerDisplay = document.querySelector('.display-player');
-const resetButton = document.querySelector('#reset');
-const announcer = document.querySelector('.announcer');
-
-tiles.forEach( (tile, index) => {
-    tile.addEventListener('click', () => userAction(tile, index));
+tiles.forEach((tile, index) => {
+  tile.addEventListener("click", () => userAction(tile, index));
 });
 
-resetButton.addEventListener('click', resetBoard);
+resetButton.addEventListener("click", resetBoard);
 
-/// WRITE YOUR CODE AFTER THIS LINE
+let currentPlayer = "X";
+let running = false;
+let robotPlayer = false;
+
+function getEmptyBoard() {
+  let board = [
+    [".", ".", "."],
+    [".", ".", "."],
+    [".", ".", "."],
+  ];
+}
+tiles[0] = ".";
+
+function getPlayerMove(tiles, currentPlayer) {
+  tiles.add(currentPlayer);
+}
+let turn = false;
+function changePlayer() {
+    moves++;
+    if (currentPlayer == "X") {
+      
+        currentPlayer = "0";
+        moves++;
+  } else {
+    currentPlayer = "X";
+  }
+
+  announcer.textContent = `${currentPlayer}s turn`;
+}
 
 function userAction(tile, index) {
-    // TODO Handle when the user clicks on a tile
-}
+    console.log(index);
+    if (!tile.textContent) {
+        tile.textContent = `${currentPlayer}`;
+        console.log(tiles[index].innerHTML);
+        changePlayer();
+    }
+  // TODO Handle when the user clicks on a tile
+} let place = 0;
+function isBoardFull(tiles) {
+    for (let i = 0; i <= 8; i++)
+        if (tiles[i].innerHTML == "X" || tiles[i].innerHTML == "X")
+            place++;
+    if (place == 9)
+    console.log("Draw!")
+
+        }
+
+
+
+isBoardFull();
 
 function resetBoard() {
-    // TODO Empty the board
+  // TODO Empty the board
 }
+
+function getRandomAIMove() {
+  let i = Math.floor(Math.random() * tiles.lenght);
+}
+console.log(tiles);
+console.log(tiles.values);
